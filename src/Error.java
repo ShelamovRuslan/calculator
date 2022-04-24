ece7ed5a2ad19f99e4a36b0a9e1ee92896575f81
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-04-24 05:06:24 +0300

    calculator

--- src/Error.java
+++ src/Error.java
@@ -1,18 +1,15 @@
 public class Error {
 
     public static void singError() {
-        System.out.println("В вашем выражение неверно расставлены знаки. Введите корректное выражение.");
-        RunCalc.runCalc();
+        Calculator.core("В вашем выражение неверно расставлены знаки. Введите корректное выражение.");
     }
 
     public static void parenthesesError() {
-        System.out.println("В вашем выражение неверно раставлены скобки. Введите корректное выражение.");
-        RunCalc.runCalc();
+        Calculator.core("В вашем выражение неверно раставлены скобки. Введите корректное выражение.");
     }
 
     public static void incorrectSymbolError() {
-        System.out.println("В вашем выражение присутствуют недопустимые символы. Введите корректное выражение.");
-        RunCalc.runCalc();
+        Calculator.core("В вашем выражение присутствуют недопустимые символы. Введите корректное выражение.");
     }
 
 }
